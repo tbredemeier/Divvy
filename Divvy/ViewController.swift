125160259be4a16c4961174e2f95891793d7709d
Tom Bredemeier <tbredemeier@barrington220.org>
2018-05-08 17:57:00 -0500

    added Divvy bike station annotations

--- Divvy/ViewController.swift
+++ Divvy/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     var myLocation = CLLocation()
     let locationManager = CLLocationManager()
+    let query = "https://feeds.divvybikes.com/stations/stations.json"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +35,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let span = MKCoordinateSpanMake(0.01, 0.01)
         let region = MKCoordinateRegionMake(center, span)
         mapView.setRegion(region, animated: true)
+        if let url = URL(string: self.query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                self.parse(json: json)
+                return
+            }
+        }
+        loadError()
     }
+
+    func parse(json: JSON) {
+        for result in json["stationBeanList"].arrayValue {
+            let stationName = result["stationName"].stringValue
+            let latitude = result["latitude"].doubleValue
+            let longitude = result["longitude"].doubleValue
+            let location = CLLocation(latitude: latitude,
+                                      longitude: longitude)
+            let annotation = MKPointAnnotation()
+            annotation.title = stationName
+            annotation.coordinate = location.coordinate
+            mapView.addAnnotation(annotation)
+        }
+    }
+
+    func loadError() {
+        let alert = UIAlertController(title: "Loading Error", message:
+            "There was a problem loading the Divvy bike data",
+                                      preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+
 
 }
 
